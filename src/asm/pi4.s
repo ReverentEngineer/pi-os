4662fe27511f0a36d2e75e89787df5407de90f66
Jeffrey Caffrey-Hill <jeff@caffreyhill.com>
2023-01-16 08:00:02 -0500

    Park all but the primary core

--- src/asm/pi4.s
+++ src/asm/pi4.s
@@ -2,6 +2,12 @@
 .global _start
     .org 0x80000
 _start:
+    mrs	    x0, mpidr_el1
+    and     x0, x0, #3
+    ldr     x1, =0
+    cmp     x0, x1
+    b.ne    parked
+
     ldr     x5, =_start
     mov     sp, x5
     
@@ -16,3 +22,7 @@ _start:
  
 2:
     b      kmain
+
+parked:
+    wfe
+    b      parked
