8cf98afce56b7aebefaf2c17ef49b1b78083ae88
Jeffrey Caffrey-Hill <jeff@caffreyhill.com>
2023-01-16 08:22:35 -0500

    Improve comments in boot assembly

--- src/asm/pi4.s
+++ src/asm/pi4.s
@@ -2,13 +2,15 @@
 .global _start
     .org 0x80000
 _start:
+    // Park all but core "0"
     mrs	    x0, mpidr_el1
     and     x0, x0, #3
     ldr     x1, =0
     cmp     x0, x1
     b.ne    parked
 
-    // clear bss
+   
+    // Clear bss
     ldr     x5, =__bss_start
     ldr     w6, =__bss_size
 1:
@@ -18,11 +20,14 @@ _start:
     cbnz    w6, 1b
  
 2:
+    // Setup stack
     ldr     x5, =_start
     mov     sp, x5
   
+    /// Jump to Rust entry point
     b      kmain
 
 parked:
+    /// Park not primary cores 
     wfe
     b      parked
