d5a2364d2588e386b1645976c9c27aacebefa720
Jeffrey Caffrey-Hill <jeff@caffreyhill.com>
2023-01-15 19:44:22 -0500

    Remove unused code

--- src/mailbox.rs
+++ src/mailbox.rs
@@ -13,12 +13,3 @@ pub fn status() -> Status {
     unsafe { Status::from_bits_unchecked(mmio::Register::MBOX_STATUS.read()) }
 }
 
-
-#[derive(serde::Serialize)]
-pub enum Tag {
-    SetClockRate {
-        clock_id: u32, //
-        rate: u32, // Hz
-        skip_setting_turbo: u32
-    }
-}
