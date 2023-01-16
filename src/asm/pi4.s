665a16675d4ddb104d902a97ec08f63ce2201b46
Jeffrey Caffrey-Hill <jeff@caffreyhill.com>
2023-01-16 07:27:08 -0500

    Clean-up boot code

--- src/asm/pi4.s
+++ src/asm/pi4.s
@@ -1,20 +1,18 @@
-.section ".text.boot"
- 
-.globl _start
- 
+.section .text.boot
+.global _start
     .org 0x80000
 _start:
     ldr     x5, =_start
     mov     sp, x5
- 
+    
+    // clear bss
     ldr     x5, =__bss_start
     ldr     w6, =__bss_size
-1:  cbz     w6, 2f
+1:
+    cbz     w6, 2f
     str     xzr, [x5], #8
     sub     w6, w6, #1
     cbnz    w6, 1b
  
-2:  bl      kmain
-halt:
-    wfe
-    b halt
+2:
+    b      kmain
